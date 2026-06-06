707cdde44a669479dcdb6e3f2c33cef631479af7
Manish Ambre <manishambre5@gmail.com>
2026-06-06 22:29:46 +0200

    fix: magazine layout on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -151,16 +151,22 @@ export default function App() {
 
 		) : (
 
-			<div id="magazine" className="flex justify-center items-center">
-				<div className="relative max-w-200svw max-h-200svh inline-block">
+			<div id="magazine" className="flex justify-center items-center overflow-auto">
+				<div className="relative max-w-300svw max-h-300svh inline-block scale-32 md:scale-100 origin-top">
 					<img
 						src={data?.image.image.source}
 						alt="Featured Picture"
-						className="block max-w-[200svw] max-h-[200svh]"
+						className={`block max-w-[300svw] md:max-w-[200svw] lg:max-w-screen`}
 					/>
-					<div className="absolute inset-0 bg-black/20" />
+					<div className="absolute inset-0 h-max w-max bg-black/20" />
 
-					<div className="absolute inset-0 flex flex-col m-2 justify-between">
+					<div
+						className={`absolute inset-0 flex flex-col p-2 justify-between gap-2 w-[300svw] md:w-[200svw] lg:w-screen
+						${!featPictLandscapeCheck
+							? ("")
+							: ("")
+						}
+					`}>
 
 						<header className="w-full p-4 flex flex-col items-center justify-center gap-4">
 							<div className="text-sm tracking-tight text-background flex gap-2 items-center">
@@ -173,72 +179,80 @@ export default function App() {
 
 						<p className="text-center text-bold text-background font-mono">(layout under construction...)</p>
 
-						<Item className="max-w-64 text-background w-fit relative bg-white/10">
-							<ViewFinderBorder />
-							<ItemDescription className="uppercase">
-								<Badge variant="outline" className="text-accent">
-									<StarIcon />
-									Featured Article
-								</Badge>
-							</ItemDescription>
-							<ItemTitle className="text-4xl leading-normal">{data?.tfa?.titles.normalized}</ItemTitle>
-						</Item>
+						<div className="flex flex-row justify-between gap-2 flex-1">
+							<div className="flex flex-col justify-around gap-2">
 
-						<div className="max-w-64 text-background w-fit self-end relative bg-white/10 p-4">
-							<ViewFinderBorder />
-							<p className="text-right uppercase">
-								<Badge variant="outline" className="text-accent">
-									<StarIcon />
-									Most Read
-								</Badge>
-							</p>
-							{data?.mostread?.articles.slice(0,5).map((article, i) => (
-								<Item size="xs" key={i} className="shrink-0 flex-nowrap text-nowrap text-right justify-end">
-									<ItemTitle className="font-medium text-sm">
-										<a href={article.content_urls.desktop.page}>
-											{article.title.replace(/_/g, ' ')}
-										</a>
-									</ItemTitle>
-									<ItemDescription className="text-accent text-xs font-mono">#0{i + 1}</ItemDescription>
+								<Item className="max-w-1/2 text-background w-fit relative bg-white/10">
+									<ViewFinderBorder />
+									<ItemDescription className="uppercase">
+										<Badge variant="outline" className="text-accent">
+											<StarIcon />
+											Featured Article
+										</Badge>
+									</ItemDescription>
+									<ItemTitle className="text-4xl leading-normal">{data?.tfa?.titles.normalized}</ItemTitle>
 								</Item>
-								))
-							}
-						</div>
 
-						<div className="max-w-64 text-background w-fit relative bg-white/10 p-4">
-							<ViewFinderBorder />
-							<p className="uppercase">
-								<Badge variant="outline" className="text-accent">
-									<StarIcon />
-									News
-								</Badge>
-							</p>
-							<div className="">
-							{data?.news?.slice(0,2).map((item, i) => {
-							return (
-								<Item size="sm" key={i}>
+								<div className="max-w-1/2 text-background w-fit relative bg-white/10 p-4">
+									<ViewFinderBorder />
+									<p className="uppercase">
+										<Badge variant="outline" className="text-accent">
+											<StarIcon />
+											News
+										</Badge>
+									</p>
+									<div className="">
+									{data?.news?.slice(0,2).map((item, i) => {
+									return (
+										<Item size="sm" key={i}>
+											<ItemContent>
+												<div className="lg:text-sm">
+													{parser(item.story).content}
+												</div>
+											</ItemContent>
+										</Item>
+									);
+									})}
+									</div>
+									<p className="text-right">...and more</p>
+								</div>
+
+							</div>
+							<div className="flex flex-col justify-between gap-2">
+								<div className="max-w-1/2 text-background w-fit self-end relative bg-white/10 p-4">
+									<ViewFinderBorder />
+									<p className="text-right uppercase">
+										<Badge variant="outline" className="text-accent">
+											<StarIcon />
+											Most Read
+										</Badge>
+									</p>
+									{data?.mostread?.articles.slice(0,5).map((article, i) => (
+										<Item size="xs" key={i} className="shrink-0 flex-nowrap text-nowrap text-right justify-end">
+											<ItemTitle className="font-medium text-sm">
+												<a href={article.content_urls.desktop.page}>
+													{article.title.replace(/_/g, ' ')}
+												</a>
+											</ItemTitle>
+											<ItemDescription className="text-accent text-xs font-mono">#0{i + 1}</ItemDescription>
+										</Item>
+										))
+									}
+								</div>
+
+								<Item className="max-w-1/3 text-right self-end mt-aut bg-white/10 relative">
+									<ViewFinderBorder />
 									<ItemContent>
-										<div className="lg:text-sm">
-											{parser(item.story).content}
-										</div>
+										{data && <>
+											<p className="text-background italic text-sm">{parser(data?.image?.description.html).content}</p>
+											<p className="text-background">By {parser(data?.image.artist.html).content}</p>
+											</>
+										}
 									</ItemContent>
 								</Item>
-							);
-							})}
 							</div>
-							<p className="text-right">...and more</p>
 						</div>
 
-						<Item className="max-w-96 text-right self-end mt-aut bg-white/10 relative">
-							<ViewFinderBorder />
-							<ItemContent>
-								{data && <>
-									<p className="text-background italic text-sm">{parser(data?.image?.description.html).content}</p>
-									<p className="text-background">By {parser(data?.image.artist.html).content}</p>
-									</>
-								}
-							</ItemContent>
-						</Item>
 
 					</div>
 				</div>
